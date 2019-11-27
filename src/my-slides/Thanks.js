0576f71dbe21612ded982e8328b2a6e5ba75873e
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-11-27 11:14:25 +0100

    .

--- src/my-slides/Thanks.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MarkdownSlide from "../slide-wrappers/MarkdownSlide";
-
-export default () => {
-  const markdown = `
-# &lt;Thanks /&gt; 🧔🏻👍🏻
-  `;
-  return <MarkdownSlide markdown={markdown} />;
-};
--- src/my-slides/index.js
+++ src/my-slides/index.js
@@ -7,94 +7,11 @@ import ExampleUseContext from "./ExampleUseContext";
 import ExampleRouting from "./ExampleRouting";
 import ExampleUseRef from "./ExampleUseRef";
 
-// Exercises:
-import ExerciseUseState from "./ExerciseUseState";
-import ExerciseRouting from "./ExerciseRouting";
-import ExerciseUseReducer from "./ExerciseUseReducer";
-import ExerciseContext from "./ExerciseContext";
-import ExerciseCustomHooks from "./ExerciseCustomHooks";
-
-// My slides:
-import AndYou from "./AndYou";
-import IAmBouwe from "./IAmBouwe";
-import LetsBuild from "./LetsBuild";
-import Pics from "./Pics";
-import Thanks from "./Thanks";
-import ToDoToday from "./ToDoToday";
-import Welcome from "./Welcome";
-import Hooks1 from "./Hooks1";
-import Hooks2 from "./Hooks2";
-import Hooks3 from "./Hooks3";
-import CustomHooks from "./CustomHooks";
-import UseEffect from "./UseEffect";
-import React1 from "./React1";
-import React2 from "./React2";
-import GoodbyeOldStuff from "./GoodbyeOldStuff";
-import UseReducer from "./UseReducer";
-import UseReducer2 from "./UseReducer2";
-import UseContext from "./UseContext";
-import DoYouHazTheCodez from "./DoYouHazTheCodez";
-import BeforeHooks1 from "./BeforeHooks1";
-import BeforeHooks2 from "./BeforeHooks2";
-import Context from "./Context";
-import ContextIsNiceBut from "./Context2";
-import ColocatingState from "./ColocatingState";
-import UseRef from "./UseRef";
-import ThatsIt from "./ThatsIt";
-import Break from "./Break";
-import NewNexus1 from "./NewNexus1";
-import NewNexus2 from "./NewNexus2";
-import NewNexus3 from "./NewNexus3";
-import NewNexusBegin from "./NewNexusBegin";
-import NewNexusEnd from "./NewNexusEnd";
-import Feedback from "./Feedback";
-
 export default [
-  Welcome,
-  IAmBouwe,
-  NewNexusBegin,
-  NewNexus1,
-  NewNexus2,
-  NewNexus3,
-  NewNexusEnd,
-  AndYou,
-  Pics,
-  ToDoToday,
-  LetsBuild,
-  React1,
-  React2,
   ExampleReact101,
-  React2,
-  BeforeHooks1,
-  BeforeHooks2,
-  Hooks1,
-  GoodbyeOldStuff,
-  Hooks2,
-  Hooks3,
-  DoYouHazTheCodez,
-  ExerciseUseState,
-  Break,
-  UseReducer,
-  UseReducer2,
   ExampleUseReducer,
-  ExerciseUseReducer,
-  Break,
-  Context,
-  UseContext,
   ExampleUseContext,
-  ExerciseContext,
-  Break,
-  ContextIsNiceBut,
-  ColocatingState,
-  UseEffect,
   ExampleUseEffect,
-  CustomHooks,
   ExampleCustomHooks,
-  ExerciseCustomHooks,
-  Break,
-  UseRef,
-  ExampleUseRef,
-  ThatsIt,
-  Feedback,
-  Thanks
+  ExampleUseRef
 ];
